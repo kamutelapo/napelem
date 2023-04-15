623466b01dadc228116d365fb83fedb86fd1a859
Karai Csaba <cskarai@freemail.hu>
2023-04-15 07:44:10 +0200

    Auto-generated commit [ci skip]

--- adat_letöltés.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-BASEDIR=`dirname $0`
-
-"$BASEDIR/szkriptek/eon_adat_letöltés.sh"
-"$BASEDIR/szkriptek/inverter_adat_letöltés.sh"
-"$BASEDIR/szkriptek/eon_inverter_adat_egyesítés.py"
-"$BASEDIR/szkriptek/eon_grafikon_építés.sh"
-"$BASEDIR/szkriptek/inverter_grafikon_építés.sh"
